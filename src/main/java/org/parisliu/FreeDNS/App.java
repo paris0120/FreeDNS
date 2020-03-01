8c0026953ef5f1ddb0cc3da259ba00936b8dc594
Paris Liu <paris0120@gmail.com>
2020-03-01 14:32:14 -0500

    initial commit

---
+++ src/main/java/org/parisliu/FreeDNS/App.java
@@ -0,0 +1,13 @@
+package org.parisliu.FreeDNS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
